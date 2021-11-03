2cda85428b8097772ba768604978b156dd1c4082
David <david.llobet6@gmail.com>
2021-11-03 17:49:09 +0100

    Create functions for each operator

---
+++ operators.js
@@ -0,0 +1,9 @@
+const sum = (num1, num2) => num1 + num2;
+const subtraction = (num1, num2) => num1 - num2;
+const multi = (num1, num2) => num1 * num2;
+const divi = (num1, num2) => num1 / num2;
+
+module.exports = sum;
+module.exports = subtraction;
+module.exports = multi;
+module.exports = divi;
