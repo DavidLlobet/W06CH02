03bfdba32b8c2274dddca78ed9f9374eff04b02d
David <david.llobet6@gmail.com>
2021-11-03 20:06:28 +0100

    Added calculator with url values

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+
 const http = require("http");
+
+const { sum, subtraction, multi, divi } = require("./operators");
 
 const server = http.createServer();
 
@@ -9,10 +12,34 @@ server.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
 });
 
+const htmlCalculator = (num1, num2) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+      <h1>Resultados:</h1>
+  <ul>
+    <li>${num1} + ${num2} = ${sum(num1, num2)}</li>
+    <li>${num1} - ${num2} = ${subtraction(num1, num2)}</li>
+    <li>${num1} * ${num2} = ${multi(num1, num2)}</li>
+    <li>${num1} / ${num2} = ${divi(num1, num2)}</li>
+  </ul>
+  </body>
+</html>`;
+
 server.on("request", (request, response) => {
-  console.log("request.url");
+  const myUrl = new URL(request.url, `http://${request.headers.host}`);
+  const myUrlValues = myUrl.searchParams.values();
+  const numberValues = [];
+  for (const value of myUrlValues) {
+    numberValues.push(+value);
+  }
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
-  response.write("<h1>BULEANO</h1>");
+  response.write(htmlCalculator(numberValues[0], numberValues[1]));
   response.end();
 });
